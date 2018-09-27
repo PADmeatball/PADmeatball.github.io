0a45d7dca4b7a6cbc473f6e09150f0c0c559d7c1
PADmeatball <43628660+PADmeatball@users.noreply.github.com>
2018-09-27 14:23:43 +0900

    Add files via upload

---
+++ BGMchanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMchanger : MonoBehaviour {
+
+    
+    private bool pitchflag;
+    private float timer;
+
+    private AudioSource MainBGM;
+    
+
+    // Use this for initialization
+    void Start () {
+    
+        timer = 45.0f;
+        MainBGM = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+
+       if(timer <=15.0f)
+        {
+            MainBGM.pitch = 1.2f;
+        }
+	}
+}
